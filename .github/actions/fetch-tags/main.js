84e7e565575618fd0c2d74312378ec73b2dbf66c
Gustavo Perdomo <gperdomor@gmail.com>
2022-04-08 18:03:52 -0400

    chore: reorganization and tweaks (#2)
    * chore: reorganization and build automation

---
+++ .github/actions/fetch-tags/main.js
@@ -0,0 +1,62 @@
+const { setOutput, setFailed, info, getInput } = require('@actions/core');
+const { fetch, Headers } = require('undici');
+const semver = require('semver');
+
+const getAuthToken = async (repository) => {
+  const url = `https://auth.docker.io/token?service=registry.docker.io&scope=repository:${repository}:pull,push`;
+  const response = await fetch(url);
+
+  if (response.ok) {
+    return await response.json();
+  }
+
+  throw new Error('Failed to fetch authentication token.');
+};
+
+const getTags = async (repository) => {
+  const { token } = await getAuthToken(repository);
+  const url = `https://registry.hub.docker.com/v2/${repository}/tags/list`;
+  const response = await fetch(url, {
+    headers: new Headers({
+      authorization: `Bearer ${token}`,
+    }),
+  });
+
+  if (response.ok) {
+    return await response.json();
+  }
+
+  throw new Error('Failed to fetch tags from Docker Hub.');
+};
+
+const main = async () => {
+  try {
+    const ignoredTags = getInput('ignored-tags').split(',');
+
+    const { tags: nodeTags } = await getTags('library/node');
+    const { tags: builderTags } = await getTags('gperdomor/nx-docker');
+
+    const regex = /^(14|16|18)\.\d{1,2}\.\d{1,2}-alpine$/;
+
+    const toBuild = new Set(
+      nodeTags.filter((tag) => regex.test(tag) && !ignoredTags.includes(tag))
+    );
+    const currentTags = builderTags.filter((tag) => regex.test(tag));
+
+    currentTags.forEach((tag) => {
+      toBuild.delete(tag);
+    });
+
+    const sortedTags = semver.sort([...toBuild]);
+    const latest = sortedTags?.slice(-1)?.[0] || '';
+
+    info(`Calculated tags to build:\n${sortedTags.join('\n')}`);
+
+    setOutput('missing-tags', sortedTags);
+    setOutput('latest', latest.startsWith('16') ? latest : '');
+  } catch (error) {
+    setFailed(error.message);
+  }
+};
+
+main();
